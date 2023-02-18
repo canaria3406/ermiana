bc96b46af5b221daa06a5975e416e2e233547b97
canaria3406 <27627468+canaria3406@users.noreply.github.com>
2023-02-18 23:57:57 +0800

    routine task time update.

--- common/runBahaCron.js
+++ common/runBahaCron.js
@@ -2,7 +2,7 @@ import { CronJob } from 'cron';
 import { reloadBahaTK } from "./reloadBahaTK.js";
 
 export function runBahaCron() {
-    const job = new CronJob('00 48 17 * * 6', function() {
+    const job = new CronJob('00 30 15 * * 2', function() {
         console.log('Cronjob running...');
         reloadBahaTK();
     }, null, true, 'Asia/Taipei');
