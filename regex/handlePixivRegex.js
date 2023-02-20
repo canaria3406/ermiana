607e24a6f01ac19d8deded0d05338aec9e383364
canaria3406 <cy4v.tw@gmail.com>
2023-02-20 11:58:43 +0800

    Fix Pixiv embed.

--- regex/handlePixivRegex.js
+++ regex/handlePixivRegex.js
@@ -17,8 +17,8 @@ export async function handlePixivRegex( result, message ){
         pixivEmbed.setColor(2210780);
         pixivEmbed.setTitle(resp.data.body.title);
         pixivEmbed.setURL(result[0]);
+        pixivEmbed.setDescription(resp.data.body.extraData.meta.twitter.description);
         pixivEmbed.addFields(
-            { name: '簡介', value: resp.data.body.extraData.meta.twitter.description},
             { name: '作者', value: resp.data.body.userName, inline : true},
             { name: '收藏', value: resp.data.body.bookmarkCount.toString(), inline : true},
             { name: '標籤', value: tagString}
