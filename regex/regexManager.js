8e274ac7f216f4978e4eb367191f9d0056fd03c1
canaria3406 <cy4v.tw@gmail.com>
2023-02-22 22:41:41 +0800

    Update regexManager.

--- regex/regexManager.js
+++ regex/regexManager.js
@@ -5,14 +5,19 @@ import { handlePixivRegex } from "./handlePixivRegex.js";
 import { handlePlurkRegex } from "./handlePlurkRegex.js";
 
 export const regexs =  [
-    { regex: /https:\/\/e(?:x|-)hentai\.org\/g\/([0-9]+)\/([0-9a-z]+)\//,
+    {
+        regex: /https:\/\/e(?:x|-)hentai\.org\/g\/([0-9]+)\/([0-9a-z]+)\//,
         handler: handleEhRegex },
-    { regex: /https?:\/\/www\.ptt\.cc\/bbs\/((?:G|g)ossiping|AC_In)\/M\.([0-9]+)\.A\.([0-9A-Z]+)\.html/,
+    {
+        regex: /https?:\/\/www\.ptt\.cc\/bbs\/((?:G|g)ossiping|AC_In)\/M\.([0-9]+)\.A\.([0-9A-Z]+)\.html/,
         handler: handlePttRegex },
-    { regex: /https?:\/\/([a-z]+)\.gamer\.com\.tw([^>])*bsn=60076([^>])*/,
+    {
+        regex: /https?:\/\/([a-z]+)\.gamer\.com\.tw([^>])*bsn=60076([^>])*/,
         handler: handleBahaRegex },
-    { regex: /https:\/\/www\.pixiv\.net\/artworks\/([0-9]+)/,
+    {
+        regex: /https:\/\/www\.pixiv\.net\/artworks\/([0-9]+)/,
         handler: handlePixivRegex },
-    { regex: /https:\/\/www\.plurk\.com\/p\/([^"]+)/,
+    {
+        regex: /https:\/\/www\.plurk\.com\/p\/([^"]+)/,
         handler: handlePlurkRegex }
 ];
