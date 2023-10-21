cc994d14a5225136848bdd1d9383836021d54b3a
canaria3406 <cy4v.tw@gmail.com>
2023-10-22 01:50:37 +0800

    switch to fx twitter api

--- regex/handleTwitterRegex.js
+++ regex/handleTwitterRegex.js
@@ -12,97 +12,144 @@ export async function handleTwitterRegex( result, message ){
     } catch{}
     
     const tid = result[1];
+
     try{
-        const ermianaTwitter = new Conf({projectName: "ermianaJS"});
-        if(!ermianaTwitter.get("parameter_token")){
-            await reloadTwitterTK();
-        }
-        const parameter_token = ermianaTwitter.get("parameter_token");
-        const header_x_csrf_token = ermianaTwitter.get("header_x_csrf_token");
-        const header_authorization = ermianaTwitter.get("header_authorization");
-        const header_cookie = ermianaTwitter.get("header_cookie");
-
-        const headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Host": "twitter.com",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
-            "Accept": "*/*",
-            "Accept-Language": "zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate, br",
-            "content-type": "application/json",
-            "x-twitter-auth-type": "OAuth2Session",
-            "x-csrf-token": header_x_csrf_token,
-            "x-twitter-client-language": "zh-tw",
-            "x-twitter-active-user": "yes",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "authorization": header_authorization,
-            "Connection": "keep-alive",
-            "Cookie": header_cookie,
-            "TE": "trailers",
-          };
-
-        const url = "https://twitter.com/i/api/graphql/" + parameter_token + "/TweetDetail?variables=%7B%22focalTweetId%22%3A%22" + tid + "%22%2C%22with_rux_injections%22%3Afalse%2C%22includePromotedContent%22%3Atrue%2C%22withCommunity%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withBirdwatchNotes%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22rweb_lists_timeline_redesign_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Afalse%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_media_download_video_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D&fieldToggles=%7B%22withArticleRichContentState%22%3Afalse%7D";
-
-        const resp = await axios.request({
+        const apiResp = await axios.request({
             method: "get",
-            url: url,
-            headers: headers
+            url: "https://api.fxtwitter.com/i/status/" + tid
         });
 
-        let result = resp.data.data.threaded_conversation_with_injections_v2.instructions[0].entries[0].content.itemContent?.tweet_results.result;
+        if (apiResp.status == 200) {
+            // use fx api
+            const apitwitterEmbed = new EmbedBuilder();
+            apitwitterEmbed.setColor(0x1DA1F2);        
+            apitwitterEmbed.setAuthor({ name: "@" + apiResp.data.tweet.author.screen_name , iconURL: apiResp.data.tweet.author.avatar_url});  
+            apitwitterEmbed.setTitle(apiResp.data.tweet.author.name);
+            apitwitterEmbed.setURL(apiResp.data.tweet.url);
 
-        if (result.rest_id != tid){
-            resp.data.data.threaded_conversation_with_injections_v2.instructions[0].entries.forEach(entry => {
-                if (entry.content.itemContent?.tweet_results.result.rest_id == tid) {
-                    result = entry.content.itemContent.tweet_results.result;
+            try{
+                apitwitterEmbed.setDescription(apiResp.data.tweet.text);
+            } catch{}
+            try {
+                if (apiResp.data.tweet.media.mosaic && apiResp.data.tweet.media.mosaic.type === "mosaic_photo") {
+                    apitwitterEmbed.setImage(apiResp.data.tweet.media.mosaic.formats.jpeg);
+                } else if (apiResp.data.tweet.media.all[0].type === "photo") {
+                    apitwitterEmbed.setImage(apiResp.data.tweet.media.all[0].url + "?name=large");
                 }
-            });
-        }
+            } catch{}
 
-        const tweetinfo = " • 💬" + result.legacy.reply_count.toString() + " 🔁" + result.legacy.retweet_count.toString() + " ❤️" + result.legacy.favorite_count.toString();
+            const apitweetinfo = " • 💬" + apiResp.data.tweet.replies.toString() + " 🔁" + apiResp.data.tweet.retweets.toString() + " ❤️" + apiResp.data.tweet.likes.toString();
+            apitwitterEmbed.setFooter({ text: "canaria3406" + apitweetinfo, iconURL: "https://cdn.discordapp.com/avatars/242927802557399040/1f3b1744568e4333a8889eafaa1f982a.png"});
 
-        const twitterEmbed = new EmbedBuilder();
-        twitterEmbed.setColor(0x1DA1F2);
-        twitterEmbed.setAuthor({ name: "@" + result.core.user_results.result.legacy.screen_name , iconURL: result.core.user_results.result.legacy.profile_image_url_https})
-        twitterEmbed.setTitle(result.core.user_results.result.legacy.name);
-        //twitterEmbed.addFields(
-        //    { name: "Likes", value: result.legacy.favorite_count.toString(), inline : true},
-        //    { name: "Retweets", value: result.legacy.retweet_count.toString(), inline : true}            
-        //);
-        twitterEmbed.setURL("https://twitter.com/i/status/" + tid);
-        
-        try{
-            twitterEmbed.setDescription(result.legacy.full_text);
-        } catch{}
-        try {
-            if(result.legacy.extended_entities.media[0].type == "photo"){
-                    twitterEmbed.setImage(result.legacy.extended_entities.media[0].media_url_https + "?name=large");
+            embedSuppresser(message);
+            messageSender(message.channel, apitwitterEmbed);
+            
+            try {
+                if(apiResp.data.tweet.media.all[0].type != "photo"){
+                    message.channel.send(apiResp.data.tweet.media.all[0].url);
+                }
             }
-        } catch{}
-        twitterEmbed.setFooter({ text: "canaria3406" + tweetinfo, iconURL: "https://cdn.discordapp.com/avatars/242927802557399040/1f3b1744568e4333a8889eafaa1f982a.png"});
+            catch {}
 
-        embedSuppresser(message);
-        messageSender(message.channel, twitterEmbed);
-        
-        try {
-            if(result.legacy.extended_entities?.media[0].type == "video" || "animated_gif"){
-                let maxBitrate = -1;
-                let maxBitrateUrl = 'error';
-                result.legacy.extended_entities.media[0].video_info.variants.forEach(variant => {
-                    if (variant.content_type === 'video/mp4' && variant.bitrate > maxBitrate) {
-                      maxBitrate = variant.bitrate;
-                      maxBitrateUrl = variant.url;
+        }
+        else {
+            // use vanilla api
+            try{
+                const ermianaTwitter = new Conf({projectName: "ermianaJS"});
+                if(!ermianaTwitter.get("parameter_token")){
+                    await reloadTwitterTK();
+                }
+                const parameter_token = ermianaTwitter.get("parameter_token");
+                const header_x_csrf_token = ermianaTwitter.get("header_x_csrf_token");
+                const header_authorization = ermianaTwitter.get("header_authorization");
+                const header_cookie = ermianaTwitter.get("header_cookie");
+
+                const headers = {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Host": "twitter.com",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
+                    "Accept": "*/*",
+                    "Accept-Language": "zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "content-type": "application/json",
+                    "x-twitter-auth-type": "OAuth2Session",
+                    "x-csrf-token": header_x_csrf_token,
+                    "x-twitter-client-language": "zh-tw",
+                    "x-twitter-active-user": "yes",
+                    "Sec-Fetch-Dest": "empty",
+                    "Sec-Fetch-Mode": "cors",
+                    "Sec-Fetch-Site": "same-origin",
+                    "authorization": header_authorization,
+                    "Connection": "keep-alive",
+                    "Cookie": header_cookie,
+                    "TE": "trailers",
+                };
+
+                const url = "https://twitter.com/i/api/graphql/" + parameter_token + "/TweetDetail?variables=%7B%22focalTweetId%22%3A%22" + tid + "%22%2C%22with_rux_injections%22%3Afalse%2C%22includePromotedContent%22%3Atrue%2C%22withCommunity%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withBirdwatchNotes%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22rweb_lists_timeline_redesign_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Afalse%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_media_download_video_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D&fieldToggles=%7B%22withArticleRichContentState%22%3Afalse%7D";
+
+                const resp = await axios.request({
+                    method: "get",
+                    url: url,
+                    headers: headers
+                });
+
+                let result = resp.data.data.threaded_conversation_with_injections_v2.instructions[0].entries[0].content.itemContent?.tweet_results.result;
+
+                if (result.rest_id != tid){
+                    resp.data.data.threaded_conversation_with_injections_v2.instructions[0].entries.forEach(entry => {
+                        if (entry.content.itemContent?.tweet_results.result.rest_id == tid) {
+                            result = entry.content.itemContent.tweet_results.result;
+                        }
+                    });
+                }
+
+                const tweetinfo = " • 💬" + result.legacy.reply_count.toString() + " 🔁" + result.legacy.retweet_count.toString() + " ❤️" + result.legacy.favorite_count.toString();
+
+                const twitterEmbed = new EmbedBuilder();
+                twitterEmbed.setColor(0x1DA1F2);
+                twitterEmbed.setAuthor({ name: "@" + result.core.user_results.result.legacy.screen_name , iconURL: result.core.user_results.result.legacy.profile_image_url_https})
+                twitterEmbed.setTitle(result.core.user_results.result.legacy.name);
+                //twitterEmbed.addFields(
+                //    { name: "Likes", value: result.legacy.favorite_count.toString(), inline : true},
+                //    { name: "Retweets", value: result.legacy.retweet_count.toString(), inline : true}            
+                //);
+                twitterEmbed.setURL("https://twitter.com/i/status/" + tid);
+                
+                try{
+                    twitterEmbed.setDescription(result.legacy.full_text);
+                } catch{}
+                try {
+                    if(result.legacy.extended_entities.media[0].type == "photo"){
+                            twitterEmbed.setImage(result.legacy.extended_entities.media[0].media_url_https + "?name=large");
                     }
-                  });
-                message.channel.send(maxBitrateUrl);
+                } catch{}
+                twitterEmbed.setFooter({ text: "canaria3406" + tweetinfo, iconURL: "https://cdn.discordapp.com/avatars/242927802557399040/1f3b1744568e4333a8889eafaa1f982a.png"});
+
+                embedSuppresser(message);
+                messageSender(message.channel, twitterEmbed);
+                
+                try {
+                    if(result.legacy.extended_entities?.media[0].type == "video" || "animated_gif"){
+                        let maxBitrate = -1;
+                        let maxBitrateUrl = 'error';
+                        result.legacy.extended_entities.media[0].video_info.variants.forEach(variant => {
+                            if (variant.content_type === 'video/mp4' && variant.bitrate > maxBitrate) {
+                            maxBitrate = variant.bitrate;
+                            maxBitrateUrl = variant.url;
+                            }
+                        });
+                        message.channel.send(maxBitrateUrl);
+                    }
+                }
+                catch {}
+            }
+            catch{
+                console.log("twitter vanilla api error");
             }
         }
-        catch {}
 
     }
     catch{
-        console.log("twitter error");
+        console.log("fxtwitter api error");
     }
 };
