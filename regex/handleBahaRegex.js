c018179b7986dabd59beaf6af5c39daf78bf28a2
canaria3406 <cy4v.tw@gmail.com>
2023-10-22 17:34:31 +0800

    Update handleBahaRegex.js

--- regex/handleBahaRegex.js
+++ regex/handleBahaRegex.js
@@ -39,11 +39,41 @@ export async function handleBahaRegex( result, message ){
         bahaEmbed.setFooter({ text: "canaria3406", iconURL: "https://cdn.discordapp.com/avatars/242927802557399040/1f3b1744568e4333a8889eafaa1f982a.png"});
 
         embedSuppresser(message);
-
         messageSender(message.channel, bahaEmbed);
     }
     catch{
         console.log("baha error");
         await reloadBahaTK();
+        try {
+            const ermianaBH2 = new Conf({projectName: "ermianaJS"});
+            const BAHAENUR2 = ermianaBH2.get("BAHAENUR");
+            const BAHARUNE2 = ermianaBH2.get("BAHARUNE");
+
+            const pageHTML2 = await axios.request({
+                url: result[0],
+                method: "get",
+                headers: {Cookie: "BAHAENUR="+ BAHAENUR2 +"; BAHARUNE="+ BAHARUNE2 +";"} 
+            });
+
+            const $ = cheerio.load(pageHTML2.data);
+
+            const bahaEmbed2 = new EmbedBuilder();
+            bahaEmbed2.setColor(1559500);
+            bahaEmbed2.setTitle($("meta[property=og:title]").attr("content"));
+            bahaEmbed2.setURL(result[0]);
+            try {
+                bahaEmbed2.setDescription($("meta[property=og:description]").attr("content"));
+            } catch{}
+            try {
+                bahaEmbed2.setImage($("meta[property=og:image]").attr("content"));
+            } catch{}
+            bahaEmbed2.setFooter({ text: "canaria3406", iconURL: "https://cdn.discordapp.com/avatars/242927802557399040/1f3b1744568e4333a8889eafaa1f982a.png"});
+
+            embedSuppresser(message);
+            messageSender(message.channel, bahaEmbed2);
+        }
+        catch {
+            console.log("baha second try error");
+        }
     }
 };
