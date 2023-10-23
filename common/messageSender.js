3bba0df8d374174d67a79dd28f3eeb04ad3c0456
canaria3406 <cy4v.tw@gmail.com>
2023-10-23 09:05:26 +0800

    Update messageSender.js

--- common/messageSender.js
+++ common/messageSender.js
@@ -1,7 +1,7 @@
 export async function messageSender(channel, embed, textinfo) {
   try {
     const textinfo2 = textinfo || 'canaria3406';
-    embed.setFooter({ text: textinfo2, iconURL: 'https://cdn.discordapp.com/avatars/242927802557399040/1f3b1744568e4333a8889eafaa1f982a.png' });
+    embed.setFooter({ text: textinfo2, iconURL: 'https://cdn.discordapp.com/avatars/242927802557399040/14d549f14db4efece387552397433e6b.png' });
     await channel.send({ embeds: [embed] });
   } catch {
     console.log('message send error');
