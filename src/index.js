1d97299c86bcd0c5c9b4b9ff52e33e0bedb7f917
canaria3406 <cy4v.tw@gmail.com>
2023-11-08 20:35:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,17 @@ const client = new Client({
 
 client.on('ready', () =>{
   console.log(`Ready! 以 ${client.user.tag} 身分登入`);
-  console.log(`在 ${client.guilds.cache.size} 個伺服器上運作中`);
   client.user.setPresence({ status: 'dnd' });
   currentTime();
+  let totalUserCount = 0;
   client.guilds.cache.forEach( (guild) => {
-    console.log(`${guild.name} | ${guild.memberCount} | ${guild.id}`);
+    if (guild.memberCount > 100) {
+      console.log(`${guild.memberCount} | ${guild.name}`);
+    }
+    totalUserCount += guild.memberCount;
   });
+  console.log(`正在 ${client.guilds.cache.size} 個伺服器上運作中`);
+  console.log(`正在服務 ${totalUserCount} 位使用者`);
   client.user.setPresence({
     activities: [{ name: `${client.guilds.cache.size} 個伺服器的魔法詠唱`, type: ActivityType.Listening }],
     status: 'online',
