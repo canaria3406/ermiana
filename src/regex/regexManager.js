541c813bd4dd1aa519079d8b8a94c90ca83aca13
canaria3406 <cy4v.tw@gmail.com>
2023-11-21 10:35:57 +0800

    Update regexManager.js

--- src/regex/regexManager.js
+++ src/regex/regexManager.js
@@ -8,30 +8,12 @@ import { handleTwitterRegex } from './handleTwitterRegex.js';
 import { handleMisskeyRegex } from './handleMisskeyRegex.js';
 
 export const regexs = [
+  // {
+  //  regex: /https:\/\/nhentai\.net\/g\/([0-9]+)/,
+  //  handler: handleNhRegex },
   {
-    regex: /https:\/\/twitter\.com\/[A-Za-z0-9_]{1,15}\/status\/([0-9]+)/,
-    handler: handleTwitterRegex },
-  {
-    regex: /https:\/\/x\.com\/[A-Za-z0-9_]{1,15}\/status\/([0-9]+)/,
-    handler: handleTwitterRegex },
-  {
-    regex: /https:\/\/e(?:x|-)hentai\.org\/g\/([0-9]+)\/([0-9a-z]+)/,
-    handler: handleEhRegex },
-  {
-    regex: /https?:\/\/www\.ptt\.cc\/bbs\/([a-zA-Z-_]+)\/(M\.[0-9]+\.A\.[0-9A-Z]+)\.html/,
-    handler: handlePttRegex },
-  {
-    regex: /https?:\/\/m\.gamer\.com\.tw\/forum\/C\.php\?bsn=60076&snA=([0-9]+)/,
-    handler: handleBahaRegex },
-  {
-    regex: /https?:\/\/forum\.gamer\.com\.tw\/C\.php\?bsn=60076&snA=([0-9]+)/,
-    handler: handleBahaRegex },
-  {
-    regex: /https:\/\/www\.pixiv\.net\/artworks\/([0-9]+)/,
-    handler: handlePixivRegex },
-  {
-    regex: /https:\/\/www\.pixiv\.net\/en\/artworks\/([0-9]+)/,
-    handler: handlePixivRegex },
+    regex: /https:\/\/misskey\.io\/notes\/([a-zA-Z0-9]{10})/,
+    handler: handleMisskeyRegex },
   {
     regex: /https:\/\/www\.plurk\.com\/m\/p\/([a-zA-Z0-9]{3,10})/,
     handler: handlePlurkRegex },
@@ -39,9 +21,27 @@ export const regexs = [
     regex: /https:\/\/www\.plurk\.com\/p\/([a-zA-Z0-9]{3,10})/,
     handler: handlePlurkRegex },
   {
-    regex: /https:\/\/misskey\.io\/notes\/([a-zA-Z0-9]{10})/,
-    handler: handleMisskeyRegex },
-  // {
-  //  regex: /https:\/\/nhentai\.net\/g\/([0-9]+)/,
-  //  handler: handleNhRegex },
+    regex: /https?:\/\/m\.gamer\.com\.tw\/forum\/C\.php\?bsn=60076&snA=([0-9]+)/,
+    handler: handleBahaRegex },
+  {
+    regex: /https?:\/\/forum\.gamer\.com\.tw\/C\.php\?bsn=60076&snA=([0-9]+)/,
+    handler: handleBahaRegex },
+  {
+    regex: /https:\/\/e(?:x|-)hentai\.org\/g\/([0-9]+)\/([0-9a-z]+)/,
+    handler: handleEhRegex },
+  {
+    regex: /https:\/\/www\.pixiv\.net\/artworks\/([0-9]+)/,
+    handler: handlePixivRegex },
+  {
+    regex: /https:\/\/www\.pixiv\.net\/en\/artworks\/([0-9]+)/,
+    handler: handlePixivRegex },
+  {
+    regex: /https:\/\/x\.com\/[A-Za-z0-9_]{1,15}\/status\/([0-9]+)/,
+    handler: handleTwitterRegex },
+  {
+    regex: /https:\/\/twitter\.com\/[A-Za-z0-9_]{1,15}\/status\/([0-9]+)/,
+    handler: handleTwitterRegex },
+  {
+    regex: /https?:\/\/www\.ptt\.cc\/bbs\/([a-zA-Z-_]+)\/(M\.[0-9]+\.A\.[0-9A-Z]+)\.html/,
+    handler: handlePttRegex },
 ];
