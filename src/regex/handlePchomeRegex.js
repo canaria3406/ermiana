e8501189c3a6017e048cdd3d3108c4dbba1ea906
canaria3406 <cy4v.tw@gmail.com>
2023-12-15 16:43:30 +0800

    Update handlePchomeRegex.js

--- src/regex/handlePchomeRegex.js
+++ src/regex/handlePchomeRegex.js
@@ -16,43 +16,45 @@ export async function handlePchomeRegex( result, message ) {
       url: url1,
     });
 
-    // const namestr = unescape(resp1.data.match(/"Name":"([^"]+)"/)[1].replace(/\\u/g, '%u'));
-    const nickstr = unescape(resp1.data.match(/"Nick":"(.*?)",/)[1].replace(/\\u/g, '%u')).replace(/<.*?>/g, '').replace(/\\/g, '');
-    const pricestr = unescape(resp1.data.match(/"P":(\d+)/)[1].replace(/\\u/g, '%u'));
-    const picstr = unescape(resp1.data.match(/"B":"(.*?)",/)[1].replace(/\\u/g, '%u'));
-    const picurl = 'https://img.pchome.com.tw/cs' + picstr.replace(/\\/g, '');
+    if (resp1.status === 200) {
+      // const namestr = unescape(resp1.data.match(/"Name":"([^"]+)"/)[1].replace(/\\u/g, '%u'));
+      const nickstr = unescape(resp1.data.match(/"Nick":"(.*?)",/)[1].replace(/\\u/g, '%u')).replace(/<.*?>/g, '').replace(/\\/g, '');
+      const pricestr = unescape(resp1.data.match(/"P":(\d+)/)[1].replace(/\\u/g, '%u'));
+      const picstr = unescape(resp1.data.match(/"B":"(.*?)",/)[1].replace(/\\u/g, '%u'));
+      const picurl = 'https://img.pchome.com.tw/cs' + picstr.replace(/\\/g, '');
 
-    const url2 = 'https://ecapi-cdn.pchome.com.tw/cdn/ecshop/prodapi/v2/prod/' + pcid + '/desc&fields=Meta,SloganInfo&_callback=jsonp_desc?_callback=jsonp_desc';
-    const resp2 = await axios.request({
-      method: 'get',
-      url: url2,
-    });
+      const url2 = 'https://ecapi-cdn.pchome.com.tw/cdn/ecshop/prodapi/v2/prod/' + pcid + '/desc&fields=Meta,SloganInfo&_callback=jsonp_desc?_callback=jsonp_desc';
+      const resp2 = await axios.request({
+        method: 'get',
+        url: url2,
+      });
 
-    const brandstr = unescape(resp2.data.match(/BrandNames":\[(.*?)\]/)[1].replace(/\\u/g, '%u')).replace(/","/g, '_').replace(/^"|"$/g, '');
-    const sloganstr = unescape(resp2.data.match(/SloganInfo":\[(.*?)\]/)[1].replace(/\\u/g, '%u')).replace(/","/g, '\n').replace(/^"|"$/g, '');
+      const brandstr = unescape(resp2.data.match(/BrandNames":\[(.*?)\]/)[1].replace(/\\u/g, '%u')).replace(/","/g, '_').replace(/^"|"$/g, '');
+      const sloganstr = unescape(resp2.data.match(/SloganInfo":\[(.*?)\]/)[1].replace(/\\u/g, '%u')).replace(/","/g, '\n').replace(/^"|"$/g, '');
 
-    const pchomeEmbed = new EmbedBuilder();
-    pchomeEmbed.setColor(0xEA1717);
-    pchomeEmbed.setTitle(nickstr);
-    pchomeEmbed.setURL(str);
-    try {
-      pchomeEmbed.setDescription(sloganstr);
-    } catch {}
-    try {
-      pchomeEmbed.addFields(
-          { name: '品牌', value: brandstr, inline: true },
-      );
-    } catch {}
-    try {
-      pchomeEmbed.addFields(
-          { name: '價格', value: pricestr, inline: true },
-      );
-    } catch {}
-    try {
-      pchomeEmbed.setImage(picurl);
-    } catch {}
+      const pchomeEmbed = new EmbedBuilder();
+      pchomeEmbed.setColor(0xEA1717);
+      pchomeEmbed.setTitle(nickstr);
+      pchomeEmbed.setURL(str);
+      try {
+        pchomeEmbed.setDescription(sloganstr);
+      } catch {}
+      try {
+        pchomeEmbed.addFields(
+            { name: '品牌', value: brandstr, inline: true },
+        );
+      } catch {}
+      try {
+        pchomeEmbed.addFields(
+            { name: '價格', value: pricestr, inline: true },
+        );
+      } catch {}
+      try {
+        pchomeEmbed.setImage(picurl);
+      } catch {}
 
-    messageSender(message.channel, pchomeEmbed, 'ermiana');
+      messageSender(message.channel, pchomeEmbed, 'ermiana');
+    }
   } catch {
     // console.log('pchome error');
   }
