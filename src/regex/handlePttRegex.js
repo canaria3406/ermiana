0624ce8f995e21a157904a903c24234922afaa73
canaria3406 <cy4v.tw@gmail.com>
2024-01-07 15:21:56 +0800

    rewrite ptthandler

--- src/regex/handlePttRegex.js
+++ src/regex/handlePttRegex.js
@@ -19,82 +19,71 @@ export async function handlePttRegex( result, message ) {
         return boardName;
       }
     }
-    // use moptt api
-    const pttResp = await axios.request({
-      method: 'get',
-      url: 'https://moptt.tw/ptt/' + boardNameStandardization(result[1]) + '.' + result[2],
-      timeout: 2500,
-    });
-    if (pttResp.status === 200) {
-      const mopttEmbed = new EmbedBuilder();
-      mopttEmbed.setColor(2894892);
-      mopttEmbed.setTitle(pttResp.data.title);
-      mopttEmbed.setURL(pttResp.data.url);
 
-      let pttDescription = '';
+    const pttHTML = await axios.request({
+      url: result[0],
+      method: 'get',
+      headers: { Cookie: 'over18=1;' },
+      timeout: 2000,
+    });
+
+    if (pttHTML.status === 200) {
+      const $ = cheerio.load(pttHTML.data);
+      const pttHtmlTitle = $('meta[property=og:title]').attr('content') || '';
+      const pttHtmlDescription = $('meta[property=og:description]').attr('content') || '';
       try {
-        // use vanilla ptt description
-        const pttDATA = await axios.request({
-          url: result[0],
+        const pttResp = await axios.request({
           method: 'get',
-          headers: { Cookie: 'over18=1;' },
+          url: 'https://moptt.tw/ptt/' + boardNameStandardization(result[1]) + '.' + result[2],
           timeout: 2500,
         });
-        const $ = cheerio.load(pttDATA.data);
-        if ($('meta[property=og:description]').attr('content')) {
-          pttDescription = $('meta[property=og:description]').attr('content');
-        }
-      } catch {}
-      try {
-        if (pttDescription) {
-          mopttEmbed.setDescription(pttDescription);
-        } else if (pttResp.data.description) {
-          mopttEmbed.setDescription(pttResp.data.description);
-        }
-      } catch {}
-      try {
-        if (pttResp.data.imageSource) {
-          mopttEmbed.setImage(pttResp.data.imageSource);
-        }
-      } catch {}
-      if (!message.embeds[0]) {
+        const mopttEmbed = new EmbedBuilder();
+        mopttEmbed.setColor(2894892);
         try {
-          await message.channel.sendTyping();
+          if (pttResp.status === 200) {
+            mopttEmbed.setTitle(pttResp.data.title);
+          } else if (pttHtmlTitle) {
+            mopttEmbed.setTitle(pttHtmlTitle);
+          }
         } catch {}
-        messageSender(message.channel, mopttEmbed, 'ermiana');
+        mopttEmbed.setURL(result[0]);
+        try {
+          if (pttHtmlDescription !== '') {
+            mopttEmbed.setDescription(pttHtmlDescription);
+          } else if (pttResp.data.description) {
+            mopttEmbed.setDescription(pttResp.data.description);
+          }
+        } catch {}
+        try {
+          if (pttResp.data.imageSource) {
+            mopttEmbed.setImage(pttResp.data.imageSource);
+          }
+        } catch {}
+        if (!message.embeds[0]) {
+          try {
+            await message.channel.sendTyping();
+          } catch {}
+          messageSender(message.channel, mopttEmbed, 'ermiana');
+        }
+      } catch {
+        try {
+          const pttEmbed = new EmbedBuilder();
+          pttEmbed.setColor(2894892);
+          pttEmbed.setTitle(pttHtmlTitle);
+          pttEmbed.setURL(result[0]);
+          try {
+            pttEmbed.setDescription(pttHtmlDescription);
+          } catch {}
+          if (!message.embeds[0]) {
+            try {
+              await message.channel.sendTyping();
+            } catch {}
+            messageSender(message.channel, pttEmbed, 'pttermiana');
+          }
+        } catch {}
       }
     }
   } catch {
-    // console.log('moptt api error');
-    // use vanilla ptt as backup
-    try {
-      const pttHTML = await axios.request({
-        url: result[0],
-        method: 'get',
-        headers: { Cookie: 'over18=1;' },
-        timeout: 2000,
-      });
-
-      const $ = cheerio.load(pttHTML.data);
-
-      const pttEmbed = new EmbedBuilder();
-      pttEmbed.setColor(2894892);
-      pttEmbed.setTitle($('meta[property=og:title]').attr('content'));
-      pttEmbed.setURL(result[0]);
-      try {
-        if ($('meta[property=og:description]').attr('content')) {
-          pttEmbed.setDescription($('meta[property=og:description]').attr('content'));
-        }
-      } catch {}
-
-      if (!message.embeds[0]) {
-        try {
-          await message.channel.sendTyping();
-        } catch {}
-        messageSender(message.channel, pttEmbed, 'ermiana');
-      }
-    } catch {
-      console.log('ptt error: '+ message.guild.name);
-    }
+    console.log('ptt error: '+ message.guild.name);
   }
 };
