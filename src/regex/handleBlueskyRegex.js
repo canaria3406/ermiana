48a1044015713cd0498c66553dedd92e4cd6149e
canaria3406 <cy4v.tw@gmail.com>
2024-02-14 22:52:57 +0800

    Update handleBlueskyRegex.js

--- src/regex/handleBlueskyRegex.js
+++ src/regex/handleBlueskyRegex.js
@@ -13,54 +13,65 @@ export async function handleBlueskyRegex(result, message) {
       url: 'https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=' + result[1],
       timeout: 2000,
     });
+
     // console.log(didResp.data.did);
-    const threadResp = await axios.request({
-      method: 'get',
-      url: 'https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=at://' + didResp.data.did + '/app.bsky.feed.post/' + result[2],
-      timeout: 2000,
-    });
-    // console.log(threadResp.data.thread);
-    const blueskyEmbed = new EmbedBuilder();
-    blueskyEmbed.setColor(0x53b4ff);
-    try {
-      if (threadResp.data.thread.post.author?.avatar) {
-        blueskyEmbed.setAuthor({ name: '@' + result[1], iconURL: threadResp.data.thread.post.author.avatar });
-      } else {
-        blueskyEmbed.setAuthor({ name: '@' + result[1] });
-      }
-    } catch {}
-    try {
-      if (threadResp.data.thread.post.author?.displayName) {
-        blueskyEmbed.setTitle(threadResp.data.thread.post.author.displayName);
-        blueskyEmbed.setURL('https://bsky.app/profile/' + result[1] + '/post/' + result[2]);
-      } else {
-        blueskyEmbed.setTitle(result[1]);
-        blueskyEmbed.setURL('https://bsky.app/profile/' + result[1] + '/post/' + result[2]);
-      }
-    } catch {}
-    try {
-      if (threadResp.data.thread.post.record.text) {
-        blueskyEmbed.setDescription(threadResp.data.thread.post.record.text);
-      }
-    } catch {}
-    try {
-      if (threadResp.data.thread.post.embed?.images) {
-        blueskyEmbed.setImage(threadResp.data.thread.post.embed.images[0].fullsize);
-      }
-    } catch {}
+    if (didResp.status === 200) {
+      const threadResp = await axios.request({
+        method: 'get',
+        url: 'https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=at://' + didResp.data.did + '/app.bsky.feed.post/' + result[2],
+        timeout: 2000,
+      });
 
-    const threadinfo ='💬' + threadResp.data.thread.post.replyCount.toString() + ' 🔁' + threadResp.data.thread.post.repostCount.toString() + ' ❤️' + threadResp.data.thread.post.likeCount.toString();
+      // console.log(threadResp.data.thread);
+      if (threadResp.status === 200) {
+        const blueskyEmbed = new EmbedBuilder();
+        blueskyEmbed.setColor(0x53b4ff);
+        try {
+          if (threadResp.data.thread.post.author?.avatar) {
+            blueskyEmbed.setAuthor({ name: '@' + result[1], iconURL: threadResp.data.thread.post.author.avatar });
+          } else {
+            blueskyEmbed.setAuthor({ name: '@' + result[1] });
+          }
+        } catch {}
+        try {
+          if (threadResp.data.thread.post.author?.displayName) {
+            blueskyEmbed.setTitle(threadResp.data.thread.post.author.displayName);
+            blueskyEmbed.setURL('https://bsky.app/profile/' + result[1] + '/post/' + result[2]);
+          } else {
+            blueskyEmbed.setTitle(result[1]);
+            blueskyEmbed.setURL('https://bsky.app/profile/' + result[1] + '/post/' + result[2]);
+          }
+        } catch {}
+        try {
+          if (threadResp.data.thread.post.record.text) {
+            blueskyEmbed.setDescription(threadResp.data.thread.post.record.text);
+          }
+        } catch {}
+        try {
+          if (threadResp.data.thread.post.embed?.images) {
+            blueskyEmbed.setImage(threadResp.data.thread.post.embed.images[0].fullsize);
+          }
+        } catch {}
 
-    messageSender(message.channel, blueskyEmbed, threadinfo);
-    embedSuppresser(message);
+        const threadinfo ='💬' + threadResp.data.thread.post.replyCount.toString() + ' 🔁' + threadResp.data.thread.post.repostCount.toString() + ' ❤️' + threadResp.data.thread.post.likeCount.toString();
 
-    try {
-      if (threadResp.data.thread.post.embed?.images.length > 1) {
-        threadResp.data.thread.post.embed.images
-            .filter((_image, index) => index > 0)
-            .forEach((image) => message.channel.send(image.fullsize));
+        messageSender(message.channel, blueskyEmbed, threadinfo);
+        embedSuppresser(message);
+
+        try {
+          if (threadResp.data.thread.post.embed?.images.length > 1) {
+            threadResp.data.thread.post.embed.images
+                .filter((_image, index) => index > 0)
+                .forEach((image) => {
+                  const picEmbed = new EmbedBuilder();
+                  picEmbed.setColor(0x53b4ff);
+                  picEmbed.setImage(image.fullsize);
+                  messageSender(message.channel, picEmbed, 'ermiana');
+                });
+          }
+        } catch {}
       }
-    } catch {}
+    }
   } catch {
     console.log('bluesky error: '+ message.guild.name);
   }
