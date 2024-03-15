4c291b4ab732084b959470482333d1ef7d557fd1
canaria3406 <cy4v.tw@gmail.com>
2024-03-15 17:31:14 +0800

    Update handleMisskeyRegex.js

--- src/regex/handleMisskeyRegex.js
+++ src/regex/handleMisskeyRegex.js
@@ -44,14 +44,31 @@ export async function handleMisskeyRegex(result, message) {
       embedSuppresser(message);
 
       try {
-        if (resp.data.files[0]?.type == 'video/mp4') {
-          if (resp.data.files[0].url.match(/https:\/\/media\.misskeyusercontent\.com\/io\/.*\.mp4/)) {
-            message.channel.send(resp.data.files[0].url);
-          } else if (resp.data.files[0].url.match(/https:\/\/proxy\.misskeyusercontent\.com\/image\.webp\?url=.*\.mp4/)) {
-            const othersiteUrl = decodeURIComponent(resp.data.files[0].url.match(/url=(.+)/)[1]);
-            message.channel.send(othersiteUrl);
-          }
+        if (resp.data.files?.length > 1) {
+          resp.data.files
+              .filter((_file, index) => index > 0)
+              .forEach((file) => {
+                if (file.type == 'image/webp' || 'image/png' || 'image/jpg') {
+                  const noteEmbed = new EmbedBuilder();
+                  noteEmbed.setColor(0x96d04a);
+                  noteEmbed.setImage(file.url);
+                  messageSender(message.channel, noteEmbed, 'ermiana');
+                }
+              });
         }
+      } catch {}
+
+      try {
+        resp.data.files?.forEach((file) => {
+          if (file.type == 'video/mp4') {
+            if (file.url.match(/https:\/\/media\.misskeyusercontent\.com\/io\/.*\.mp4/)) {
+              message.channel.send(file.url);
+            } else if (file.url.match(/https:\/\/proxy\.misskeyusercontent\.com\/image\.webp\?url=.*\.mp4/)) {
+              const othersiteUrl = decodeURIComponent(file.url.match(/url=(.+)/)[1]);
+              message.channel.send(othersiteUrl);
+            }
+          }
+        });
       } catch {}
     } else {
       console.error('Request failed');
