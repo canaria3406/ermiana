fce809e4f0d8f4764ac9c1ce72679651bd10563d
canaria3406 <cy4v.tw@gmail.com>
2024-04-01 21:10:15 +0800

    Update handleInstagramRegex.js

--- src/regex/handleInstagramRegex.js
+++ src/regex/handleInstagramRegex.js
@@ -1,14 +1,102 @@
+import { EmbedBuilder } from 'discord.js';
+import axios from 'axios';
+import { messageSender } from '../common/messageSender.js';
 import { embedSuppresser } from '../common/embedSuppresser.js';
 
 export async function handleInstagramRegex(result, message) {
   try {
     await message.channel.sendTyping();
   } catch {}
+
+  const igid = result[1];
+
   try {
-    message.channel.send('https://www.ddinstagram.com/' + result[1] + '/');
-    await new Promise((resolve) => setTimeout(resolve, 1500));
-    embedSuppresser(message);
+    const igResp = await axios({
+      url: 'https://www.instagram.com/graphql/query/',
+      method: 'get',
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'close',
+        'Sec-Fetch-Mode': 'navigate',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+        'Referer': `https://www.instagram.com/p/${igid}/`,
+      },
+      params: {
+        query_hash: 'b3055c01b4b222b8a47dc12b090e4e64',
+        variables: JSON.stringify({ shortcode: igid }),
+      },
+      timeout: 4500,
+    });
+
+    if (igResp.status === 200) {
+      const igEmbed = new EmbedBuilder();
+      igEmbed.setColor(0xE1306C);
+      try {
+        if (igResp.data.data.shortcode_media.owner.username && igResp.data.data.shortcode_media.owner.profile_pic_url) {
+          igEmbed.setAuthor({ name: '@' + igResp.data.data.shortcode_media.owner.username, iconURL: igResp.data.data.shortcode_media.owner.profile_pic_url });
+        } else if (igResp.data.data.shortcode_media.owner.username) {
+          igEmbed.setAuthor({ name: '@' + igResp.data.data.shortcode_media.owner.username });
+        }
+      } catch {}
+      try {
+        if (igResp.data.data.shortcode_media.owner.full_name) {
+          igEmbed.setTitle(igResp.data.data.shortcode_media.owner.full_name);
+        } else {
+          igEmbed.setTitle('Instagram');
+        }
+      } catch {}
+      try {
+        if (igResp.data.data.shortcode_media.shortcode) {
+          igEmbed.setURL('https://www.instagram.com/p/' + igResp.data.data.shortcode_media.shortcode + '/');
+        } else {
+          igEmbed.setURL('https://www.instagram.com/p/' + igid + '/');
+        }
+      } catch {}
+      try {
+        if (igResp.data.data.shortcode_media.edge_media_to_caption.edges[0].node.text) {
+          igEmbed.setDescription(igResp.data.data.shortcode_media.edge_media_to_caption.edges[0].node.text);
+        }
+      } catch {}
+      try {
+        if (igResp.data.data.shortcode_media.display_url && !igResp.data.data.shortcode_media.is_video) {
+          igEmbed.setImage(igResp.data.data.shortcode_media.display_url);
+        }
+      } catch {}
+      const iginfo = '💬' + (igResp.data.data.shortcode_media.edge_media_to_comment.count?.toString() || '0') + ' ❤️' + (igResp.data.data.shortcode_media.edge_media_preview_like.count?.toString() || '0');
+      try {
+        messageSender(message.channel, igEmbed, iginfo);
+        embedSuppresser(message);
+      } catch {}
+      try {
+        if (igResp.data.data.shortcode_media.is_video) {
+          message.channel.send(`[連結](https://d.ddinstagram.com/p/${igid}/)`);
+        }
+      } catch {}
+      try {
+        if (igResp.data.data.shortcode_media.edge_sidecar_to_children.edges) {
+          igResp.data.data.shortcode_media.edge_sidecar_to_children.edges
+              .filter((_edge, index) => index > 0 && index < 4)
+              .forEach((edge) => {
+                if (!edge.node.is_video) {
+                  const picEmbed = new EmbedBuilder();
+                  picEmbed.setColor(0xE1306C);
+                  picEmbed.setImage(edge.node.display_url);
+                  messageSender(message.channel, picEmbed, 'ermiana');
+                }
+              });
+        }
+      } catch {}
+    } else {
+      throw new Error();
+    }
   } catch {
-    console.log('weibo error: '+ message.guild.name);
+    try {
+      message.channel.send('https://www.ddinstagram.com/p/' + result[1] + '/');
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      embedSuppresser(message);
+    } catch {
+      console.log('instagram error: '+ message.guild.name);
+    }
   }
 };
