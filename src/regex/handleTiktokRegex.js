dee779d38787f942b3133b4237dd291602a1719f
canaria3406 <cy4v.tw@gmail.com>
2024-04-02 20:10:09 +0800

    Create handleTiktokRegex.js

---
+++ src/regex/handleTiktokRegex.js
@@ -0,0 +1,14 @@
+import { embedSuppresser } from '../common/embedSuppresser.js';
+
+export async function handleTiktokRegex(result, message) {
+  try {
+    await message.channel.sendTyping();
+  } catch {}
+  try {
+    message.channel.send(result[0].replace(/tiktok\.com/, 'tnktok.com'));
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    embedSuppresser(message);
+  } catch {
+    console.log('tiktok error: '+ message.guild.name);
+  }
+};
