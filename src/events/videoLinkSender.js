88d04bb65d159d8935635dd824fdfabcf8ced3a8
canaria3406 <cy4v.tw@gmail.com>
2024-04-09 19:08:53 +0800

    Create videoLinkSender.js

---
+++ src/events/videoLinkSender.js
@@ -0,0 +1,7 @@
+export async function videoLinkSender(message, videoLink) {
+  try {
+    await message.channel.send('[連結](' + videoLink +')');
+  } catch {
+    // console.log('videoLink send error');
+  }
+}
