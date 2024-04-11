2b02426abc26e1b37d9d02b85b53c07eb0e2edef
canaria3406 <cy4v.tw@gmail.com>
2024-04-11 17:33:59 +0800

    Update embedSuppresser.js

--- src/events/embedSuppresser.js
+++ src/events/embedSuppresser.js
@@ -1,7 +1,6 @@
 export async function embedSuppresser(message) {
   try {
     if (message.deletable) {
-      // console.log('suppressed');
       await message.suppressEmbeds(true);
     }
   } catch {
