b0238f0f4d6bbe97fdf57b78f750e7704b3baa84
canaria3406 <cy4v.tw@gmail.com>
2024-04-11 17:34:13 +0800

    Update videoLinkSender.js

--- src/events/videoLinkSender.js
+++ src/events/videoLinkSender.js
@@ -1,6 +1,6 @@
 export async function videoLinkSender(message, videoLink) {
   try {
-    await message.channel.send('[連結](' + videoLink +')');
+    await message.channel.send(`[連結](${videoLink})`);
   } catch {
     // console.log('videoLink send error');
   }
