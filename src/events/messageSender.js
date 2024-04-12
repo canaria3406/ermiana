9ca441365c187e0d2f07229cdbf023cc1fcd776b
canaria3406 <cy4v.tw@gmail.com>
2024-04-12 17:27:13 +0800

    Update messageSender.js

--- src/events/messageSender.js
+++ src/events/messageSender.js
@@ -3,6 +3,7 @@ export async function messageSender(message, embed, textinfo) {
     const textinfo2 = textinfo || 'ermiana';
     embed.setFooter({ text: textinfo2, iconURL: 'https://cdn.discordapp.com/avatars/242927802557399040/14d549f14db4efece387552397433e6b.png' });
     await message.channel.send({ embeds: [embed] });
+    // await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
   } catch {
     // console.log('message send error');
   }
