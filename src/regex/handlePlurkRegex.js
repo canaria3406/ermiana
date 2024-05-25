6b79b8169f8424fb1166d49345a745aa59a2a47a
canaria3406 <cy4v.tw@gmail.com>
2024-05-25 10:59:42 +0800

    Update handlePlurkRegex.js

--- src/regex/handlePlurkRegex.js
+++ src/regex/handlePlurkRegex.js
@@ -15,53 +15,60 @@ export async function handlePlurkRegex( result, message ) {
       timeout: 2500,
     });
 
-    const $ = cheerio.load(plurkHTML.data);
+    if (plurkHTML.status === 200) {
+      const $ = cheerio.load(plurkHTML.data);
 
-    const rePlurk = $('script').text().match(/"replurkers_count": (\d+),/)[1] || '0';
-    const favPlurk = $('script').text().match(/"favorite_count": (\d+),/)[1] || '0';
-    const respPlurk = $('script').text().match(/"response_count": (\d+),/)[1] || '0';
+      const rePlurk = $('script').text().match(/"replurkers_count": (\d+),/)[1] || '0';
+      const favPlurk = $('script').text().match(/"favorite_count": (\d+),/)[1] || '0';
+      const respPlurk = $('script').text().match(/"response_count": (\d+),/)[1] || '0';
+      const plurkInfo = '💬' + respPlurk + ' 🔁' + rePlurk + ' ❤️' + favPlurk;
 
-    const rawPlurkIndex = $('script').text().indexOf('content_raw') || -1;
-    const picPlurk = $('script').text().slice(rawPlurkIndex).match(/https:\/\/images\.plurk\.com\/[^\\"\s]+/g) || [];
+      const plurkName = $('.name').text() || '噗浪使用者';
+      const plurkContent = $('.text_holder').text() || '';
+      const rawPlurkIndex = $('script').text().indexOf('content_raw') || -1;
+      const picPlurk = $('script').text().slice(rawPlurkIndex).match(/https:\/\/images\.plurk\.com\/[^\\"\s]+/g) || [];
 
-    const plurkEmbed = new EmbedBuilder();
-    plurkEmbed.setColor(0xefa54c);
-    plurkEmbed.setTitle($('.name').text());
-    plurkEmbed.setURL('https://www.plurk.com/p/' + result[1]);
-    try {
-      plurkEmbed.setDescription($('.text_holder').text());
-    } catch {}
-    try {
-      if (picPlurk.length > 0) {
-        plurkEmbed.setImage(picPlurk[0]);
-      }
-    } catch {}
+      const plurkEmbed = new EmbedBuilder();
+      plurkEmbed.setColor(0xefa54c);
+      plurkEmbed.setTitle(plurkName);
+      plurkEmbed.setURL('https://www.plurk.com/p/' + result[1]);
+      try {
+        if (plurkContent) {
+          plurkEmbed.setDescription(plurkContent);
+        }
+      } catch {}
+      try {
+        if (picPlurk.length > 0) {
+          plurkEmbed.setImage(picPlurk[0]);
+        }
+      } catch {}
 
-    const plurkInfo = '💬' + respPlurk + ' 🔁' + rePlurk + ' ❤️' + favPlurk;
-
-    try {
-      if (!picPlurk || picPlurk.length == 0) {
-        messageSender(message, plurkEmbed, plurkInfo);
-        embedSuppresser(message);
-        await new Promise((resolve) => setTimeout(resolve, 800));
-        embedSuppresser(message);
-      } else if (picPlurk.length == 1) {
-        messageSender(message, plurkEmbed, plurkInfo);
-        embedSuppresser(message);
-        await new Promise((resolve) => setTimeout(resolve, 800));
-        embedSuppresser(message);
-      } else if (picPlurk.length > 1) {
-        const imageArray =[];
-        picPlurk.filter((_pic, index) => index > 0 && index < 4)
-            .forEach((pic) => {
-              imageArray.push(pic);
-            });
-        messageSenderMore(message, plurkEmbed, plurkInfo, imageArray);
-        embedSuppresser(message);
-        await new Promise((resolve) => setTimeout(resolve, 800));
-        embedSuppresser(message);
-      }
-    } catch {}
+      try {
+        if (!picPlurk || picPlurk.length == 0) {
+          messageSender(message, plurkEmbed, plurkInfo);
+          embedSuppresser(message);
+          await new Promise((resolve) => setTimeout(resolve, 800));
+          embedSuppresser(message);
+        } else if (picPlurk.length == 1) {
+          messageSender(message, plurkEmbed, plurkInfo);
+          embedSuppresser(message);
+          await new Promise((resolve) => setTimeout(resolve, 800));
+          embedSuppresser(message);
+        } else if (picPlurk.length > 1) {
+          const imageArray =[];
+          picPlurk.filter((_pic, index) => index > 0 && index < 4)
+              .forEach((pic) => {
+                imageArray.push(pic);
+              });
+          messageSenderMore(message, plurkEmbed, plurkInfo, imageArray);
+          embedSuppresser(message);
+          await new Promise((resolve) => setTimeout(resolve, 800));
+          embedSuppresser(message);
+        }
+      } catch {}
+    } else {
+      throw Error('plurk error: '+ message.guild.name);
+    }
   } catch {
     console.log('plurk error: '+ message.guild.name);
   }
