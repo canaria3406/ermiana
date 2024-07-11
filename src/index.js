ada9e2508f8797c3f28768eb7cf7b8e0e2c42f3b
canaria3406 <cy4v.tw@gmail.com>
2024-07-11 10:49:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const client = new Client({
 client.on('ready', () =>{
   console.log(`Ready! 以 ${client.user.tag} 身分登入`);
   currentTime();
-  client.guilds.cache
-      .filter((guild) => guild.memberCount > 500)
-      .forEach((guild) => console.log(`${guild.memberCount} | ${guild.name}`));
+  // client.guilds.cache
+  //    .filter((guild) => guild.memberCount > 500)
+  //    .forEach((guild) => console.log(`${guild.memberCount} | ${guild.name}`));
   console.log(`正在 ${client.guilds.cache.size} 個伺服器上運作中`);
   const totalUserCount = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
   console.log(`正在服務 ${totalUserCount} 位使用者`);
