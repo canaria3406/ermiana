21145487fb348bcc85fbed05f55a69803e172a6c
canaria3406 <cy4v.tw@gmail.com>
2024-07-26 15:58:16 +0800

    update reload.js

--- src/utils/runCronJob.js
+++ src/utils/runCronJob.js
@@ -1,6 +1,5 @@
 import { CronJob } from 'cron';
 import { reloadBahaTK } from './reloadBahaTK.js';
-import { refreshContextMenus } from './refreshContextMenus.js';
 
 export function runCronJob() {
   const bahaJob = new CronJob('00 30 15 * * 0,2,4,6', function() {
@@ -13,15 +12,4 @@ export function runCronJob() {
   }, null, true, 'Asia/Taipei');
   console.log('Baha Cronjob set ok!');
   bahaJob.start();
-
-  const contextMenuJob = new CronJob('00 08 14 * * 1', function() {
-    try {
-      console.log('ContextMenu Cronjob running...');
-      refreshContextMenus();
-    } catch {
-      console.log('Failed to reload context menus.');
-    }
-  }, null, true, 'Asia/Taipei');
-  console.log('ContextMenu Cronjob set ok!');
-  contextMenuJob.start();
 }
