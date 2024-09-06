60044a5cc7f18ebafbee3659fefce3ee52edc9d8
canaria3406 <cy4v.tw@gmail.com>
2024-09-06 12:43:00 +0800

    Update regexManager.js

--- src/regex/regexManager.js
+++ src/regex/regexManager.js
@@ -23,7 +23,7 @@ export const regexsMap = new Map([
   [/https:\/\/www\.plurk\.com\/m\/p\/([a-zA-Z0-9]{3,10})/, handlePlurkRegex],
   [/https:\/\/www\.plurk\.com\/p\/([a-zA-Z0-9]{3,10})/, handlePlurkRegex],
   [/https:\/\/24h\.pchome\.com\.tw\/prod\/([^?]+)/, handlePchomeRegex],
-  [/https:\/\/www\.instagram\.com\/p\/([a-zA-Z0-9-_]+)/, handleInstagramRegex],
+  [/https:\/\/www\.instagram\.com\/(?:p|reel)\/([a-zA-Z0-9-_]+)/, handleInstagramRegex],
   [/https:\/\/m\.weibo\.cn\/detail\/([0-9]+)/, handleWeiboRegex],
   [/https:\/\/bsky\.app\/profile\/([a-zA-Z0-9-.]+)\/post\/([a-zA-Z0-9]{10,16})/, handleBlueskyRegex],
   [/https:\/\/misskey\.io\/notes\/([a-zA-Z0-9]{10,16})/, handleMisskeyRegex],
