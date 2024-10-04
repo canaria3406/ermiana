835d8bce29a7dc271b2d83e988f2d0b0e467ef87
canaria3406 <cy4v.tw@gmail.com>
2024-10-04 15:47:53 +0800

    Update videoLinkSender.js

--- src/events/videoLinkSender.js
+++ src/events/videoLinkSender.js
@@ -1,9 +1,17 @@
+import { PermissionsBitField } from 'discord.js';
+
 export async function videoLinkSender(message, videoLink) {
   try {
-    try {
+    if (message.channel.permissionsFor(message.client.user).has([
+      PermissionsBitField.Flags.AttachFiles,
+    ]) ) {
       await message.channel.send({ files: [videoLink] });
-    } catch {
-      await message.channel.send(`[連結](${videoLink})`);
+    } else {
+      try {
+        await message.channel.send(`[連結](${videoLink})`);
+      } catch {
+        // console.log('videoLink backup send error');
+      }
     }
   } catch {
     // console.log('videoLink send error');
