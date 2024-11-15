f6eb7a4e571e7e97003c429d969b6b6cc0414dd3
canaria3406 <cy4v.tw@gmail.com>
2024-11-15 16:55:49 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -17,11 +17,19 @@ const client = new Client({
 client.on('ready', () =>{
   console.log(`Ready! 以 ${client.user.tag} 身分登入`);
   currentTime();
-  const serverCount = client.guilds.cache.size;
-  console.log(`正在 ${serverCount} 個伺服器上運作中`);
-  const totalUserCount = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
-  console.log(`正在服務 ${totalUserCount} 位使用者`);
-  reloadLog(serverCount, totalUserCount);
+  const promises = [
+    client.shard.fetchClientValues('guilds.cache.size'),
+    client.shard.broadcastEval((c) => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
+  ];
+  Promise.all(promises)
+      .then((results) => {
+        const serverCount = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+        const totalUserCount = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+        console.log(`正在 ${serverCount} 個伺服器上運作中`);
+        console.log(`正在服務 ${totalUserCount} 位使用者`);
+        reloadLog(serverCount, totalUserCount);
+      })
+      .catch(console.error);
 });
 
 client.on('messageCreate', async (message) => {
