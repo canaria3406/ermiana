0b57292071d55a1ee90ca51cbb85bf7148b2b2ec
canaria3406 <cy4v.tw@gmail.com>
2024-12-27 17:11:54 +0800

    Update regexManager.js

--- src/regex/regexManager.js
+++ src/regex/regexManager.js
@@ -8,9 +8,7 @@ import { handleMisskeyRegex } from './handleMisskeyRegex.js';
 import { handlePchomeRegex } from './handlePchomeRegex.js';
 import { handleBlueskyRegex } from './handleBlueskyRegex.js';
 import { handleWeiboRegex } from './handleWeiboRegex.js';
-// import { handleInstagramRegex } from './handleInstagramRegex.js';
-// [/https:\/\/www\.instagram\.com\/(?:p|reel)\/([a-zA-Z0-9-_]+)/, handleInstagramRegex],
-// [/https:\/\/www\.instagram\.com\/[A-Za-z0-9_.]+\/(?:p|reel)\/([a-zA-Z0-9-_]+)/, handleInstagramRegex],
+import { handleInstagramRegex } from './handleInstagramRegex.js';
 import { handleTiktokRegex } from './handleTiktokRegex.js';
 
 export const regexsMap = new Map([
@@ -25,6 +23,8 @@ export const regexsMap = new Map([
   [/https:\/\/www\.plurk\.com\/m\/p\/([a-zA-Z0-9]{3,10})/, handlePlurkRegex],
   [/https:\/\/www\.plurk\.com\/p\/([a-zA-Z0-9]{3,10})/, handlePlurkRegex],
   [/https:\/\/24h\.pchome\.com\.tw\/prod\/([^?]+)/, handlePchomeRegex],
+  [/https:\/\/www\.instagram\.com\/(?:p|reel)\/([a-zA-Z0-9-_]+)/, handleInstagramRegex],
+  [/https:\/\/www\.instagram\.com\/[A-Za-z0-9_.]+\/(?:p|reel)\/([a-zA-Z0-9-_]+)/, handleInstagramRegex],
   [/https:\/\/m\.weibo\.cn\/detail\/([0-9]+)/, handleWeiboRegex],
   [/https:\/\/bsky\.app\/profile\/([a-zA-Z0-9-.]+)\/post\/([a-zA-Z0-9]{10,16})/, handleBlueskyRegex],
   [/https:\/\/misskey\.io\/notes\/([a-zA-Z0-9]{10,16})/, handleMisskeyRegex],
