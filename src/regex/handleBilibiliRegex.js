24df6aaaffeb0ac44b3b50f5600827a6455a592d
canaria3406 <cy4v.tw@gmail.com>
2025-02-07 23:03:09 +0800

    Update handleBilibiliRegex.js

--- src/regex/handleBilibiliRegex.js
+++ src/regex/handleBilibiliRegex.js
@@ -19,52 +19,86 @@ export async function handleBilibiliRegex( result, message, spoiler ) {
     });
 
     if (biliHTML.status === 200) {
-      const picBilibili = biliHTML.data.data.item.modules.module_dynamic.major?.draw ? biliHTML.data.data.item.modules.module_dynamic.major.draw?.items : [];
-      const bilibiliEmbed = new EmbedBuilder();
-      bilibiliEmbed.setColor(0x00aeec);
-      try {
-        bilibiliEmbed.setAuthor({
-          name: biliHTML.data.data.item.modules.module_author.mid.toString(),
-          iconURL: biliHTML.data.data.item.modules.module_author.face,
-        });
-      } catch {}
-      try {
-        bilibiliEmbed.setTitle(biliHTML.data.data.item.modules.module_author.name.toString());
-      } catch {}
-      bilibiliEmbed.setURL(result[0]);
+      const typeBilibili = biliHTML.data.data.item.modules.module_dynamic.major.type;
+      if (typeBilibili === 'MAJOR_TYPE_DRAW') {
+        const picBilibili = biliHTML.data.data.item.modules.module_dynamic.major?.draw ? biliHTML.data.data.item.modules.module_dynamic.major.draw?.items : [];
+        const bilibiliEmbed = new EmbedBuilder();
+        bilibiliEmbed.setColor(0x00aeec);
+        try {
+          bilibiliEmbed.setAuthor({
+            name: biliHTML.data.data.item.modules.module_author.mid.toString(),
+            iconURL: biliHTML.data.data.item.modules.module_author.face,
+          });
+        } catch {}
+        try {
+          bilibiliEmbed.setTitle(biliHTML.data.data.item.modules.module_author.name.toString());
+        } catch {}
+        bilibiliEmbed.setURL(result[0]);
 
-      try {
-        if (biliHTML.data.data.item.modules.module_dynamic?.desc) {
-          bilibiliEmbed.setDescription(biliHTML.data.data.item.modules.module_dynamic.desc.text.toString());
-        }
-      } catch {}
+        try {
+          if (biliHTML.data.data.item.modules.module_dynamic?.desc) {
+            bilibiliEmbed.setDescription(biliHTML.data.data.item.modules.module_dynamic.desc.text.toString());
+          }
+        } catch {}
 
-      try {
-        if (picBilibili) {
-          bilibiliEmbed.setImage(picBilibili[0].src);
-        }
-      } catch {}
+        try {
+          if (picBilibili) {
+            bilibiliEmbed.setImage(picBilibili[0].src);
+          }
+        } catch {}
 
-      try {
-        if (!picBilibili || picBilibili.length == 0) {
-          messageSender(message, spoiler, iconURL, bilibiliEmbed, 'ermiana');
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          embedSuppresser(message);
-        } else if (picBilibili.length == 1) {
-          messageSender(message, spoiler, iconURL, bilibiliEmbed, 'ermiana');
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          embedSuppresser(message);
-        } else if (picBilibili.length > 1) {
-          const imageArray =[];
-          picBilibili.filter((_pic, index) => index > 0 && index < 4)
-              .forEach((pic) =>
-                imageArray.push(pic.src),
-              );
-          messageSenderMore(message, spoiler, iconURL, bilibiliEmbed, 'ermiana', imageArray);
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          embedSuppresser(message);
-        }
-      } catch {}
+        try {
+          if (!picBilibili || picBilibili.length == 0) {
+            messageSender(message, spoiler, iconURL, bilibiliEmbed, 'ermiana');
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            embedSuppresser(message);
+          } else if (picBilibili.length == 1) {
+            messageSender(message, spoiler, iconURL, bilibiliEmbed, 'ermiana');
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            embedSuppresser(message);
+          } else if (picBilibili.length > 1) {
+            const imageArray =[];
+            picBilibili.filter((_pic, index) => index > 0 && index < 4)
+                .forEach((pic) =>
+                  imageArray.push(pic.src),
+                );
+            messageSenderMore(message, spoiler, iconURL, bilibiliEmbed, 'ermiana', imageArray);
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            embedSuppresser(message);
+          }
+        } catch {}
+      } else if (typeBilibili === 'MAJOR_TYPE_ARTICLE') {
+        const picBilibili = biliHTML.data.data.item.modules.module_dynamic.major.article?.covers ? biliHTML.data.data.item.modules.module_dynamic.major.article?.covers[0] : '';
+        const bilibiliEmbedA = new EmbedBuilder();
+        bilibiliEmbedA.setColor(0x00aeec);
+        try {
+          bilibiliEmbedA.setAuthor({
+            name: biliHTML.data.data.item.modules.module_author.mid.toString(),
+            iconURL: biliHTML.data.data.item.modules.module_author.face,
+          });
+        } catch {}
+        try {
+          bilibiliEmbedA.setTitle(biliHTML.data.data.item.modules.module_author.name.toString());
+        } catch {}
+        bilibiliEmbedA.setURL(result[0]);
+        try {
+          if (biliHTML.data.data.item.modules.module_dynamic.major?.article) {
+            if (biliHTML.data.data.item.modules.module_dynamic.major.article.title) {
+              bilibiliEmbedA.setDescription(biliHTML.data.data.item.modules.module_dynamic.major.article.title.toString());
+            }
+          }
+        } catch {}
+        try {
+          if (picBilibili) {
+            bilibiliEmbedA.setImage(picBilibili);
+          }
+        } catch {}
+        messageSender(message, spoiler, iconURL, bilibiliEmbedA, 'ermiana');
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        embedSuppresser(message);
+      } else {
+        return;
+      }
     } else {
       return;
     }
